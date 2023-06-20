ed6e92c30bb57020812ec5f861e68a4f8f7a6875
Adib Ulinuha <50118107+uliadib12@users.noreply.github.com>
2023-06-20 21:50:36 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,49 +1,24 @@
-import socket
-import threading
+import asyncio
+import websockets
 
-def handle_client(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            if message:
-                print(f"Received message: {message}")
-                broadcast_message(message, client_socket)
-        except:
-            break
+async def handle_connection(websocket, path):
+    # Menerima pesan dari klien
+    async for message in websocket:
+        print(f"Received message: {message}")
 
-    client_socket.close()
+        # Memproses pesan (sesuai kebutuhan Anda)
+        response = f"You said: {message}"
 
-def broadcast_message(message, sender_socket):
-    for client in clients:
-        if client != sender_socket:
-            try:
-                client.send(message.encode())
-            except:
-                client.close()
-                remove_client(client)
+        # Mengirim balasan ke klien
+        await websocket.send(response)
+        print(f"Sent message: {response}")
 
-def remove_client(client_socket):
-    if client_socket in clients:
-        clients.remove(client_socket)
+# Menjalankan WebSocket server
+start_server = websockets.serve(handle_connection, 'localhost', 8765)
 
-def start_server():
-    host = '127.0.0.1'  # localhost
-    port = 12345
+async def main():
+    async with start_server:
+        print("WebSocket server started...")
+        await asyncio.Future()  # Memblokir eksekusi agar server berjalan terus
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-    print("Server started on {}:{}".format(host, port))
-
-    while True:
-        client_socket, address = server_socket.accept()
-        clients.append(client_socket)
-        print(f"Client connected: {address[0]}:{address[1]}")
-
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
-
-clients = []
-
-if __name__ == '__main__':
-    start_server()
+asyncio.run(main())
